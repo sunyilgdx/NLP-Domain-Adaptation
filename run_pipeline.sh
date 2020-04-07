b580372cdbf3d51f141825b8d000551dc9bed554
Christopher Tee <christopherfytee@gmail.com>
2020-04-07 17:18:56 -0400

    ⚗ Update configuration for DPT most similar corpus subset on linnaeus

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,35 +1,22 @@
 #!/bin/zsh
 FINE_TUNE_DATASET="linnaeus"
 PCT=2
-SEED=281
-DPT_COMPLETION=$1
-if [ -z $DPT_COMPLETION ]; then echo "Require DPT COMPLETION as first arg"; exit 1; fi
-CORPUS="data/biology/corpus/shards"
+MOD="similar"
+CORPUS="data/biology/corpus/subsets/pubmed_corpus_${MOD}_jensen-shannon_linnaeus_train_0.02pct.txt"
 FINE_TUNE_TEXT="data/biology/corpus/${FINE_TUNE_DATASET}_train.txt"
 EVAL_CORPUS="data/biology/corpus/${FINE_TUNE_DATASET}_dev.txt"
 TASK_DIR="data/biology/tasks/$FINE_TUNE_DATASET"
-OUTPUT_DIR="results/$FINE_TUNE_DATASET/pubmed_${PCT}pct_seed${SEED}_${DPT_COMPLETION}pct_dpt"
-MAX_STEPS="10000"
-
-LABELS=$TASK_DIR/labels.txt
-
+OUTPUT_DIR="results/$FINE_TUNE_DATASET/pubmed_${PCT}pct_${MOD}"
+MAX_STEPS="128194"
 
 # NER fine tuning args
 export MAX_LENGTH=128
 export NUM_EPOCHS_NER=25
 
-# Create labels if they do not exist
-if ! [ -e $LABELS ]; then
-    python -m scripts.etl.biology.tasks.extract_ner_labels $TASK_DIR
-    if [ $? -ne 0 ]; then
-        echo "Label generation failed"
-        exit 0
-    fi
-fi
-
 # Run domain adaptation
 ./domain_adaptation_pipeline.sh \
     --corpus $CORPUS \
+    --eval-corpus $EVAL_CORPUS \
     -o $OUTPUT_DIR \
     --overwrite-output-dir \
     --fine-tune-data-dir $TASK_DIR \
@@ -37,11 +24,11 @@ fi
     --batch-size 8 \
     --save-steps 2500 \
     --skip-augment-vocab \
-    --skip-domain-pre-train \
+    --skip-fine-tune \
+    --distributed-train \
     -v
 ./scripts/sync_tb_logs.sh $OUTPUT_DIR
 
 # Run end-of-training sync
-aws s3 sync $OUTPUT_DIR/fine-tuned \
-    "s3://nlp-domain-adaptation/runs/$FINE_TUNE_DATASET/$(basename $OUTPUT_DIR)/fine-tuned" \
-    --exclude "checkpoint*"
+aws s3 sync $OUTPUT_DIR/domain-pre-trained \
+    "s3://nlp-domain-adaptation/runs/$FINE_TUNE_DATASET/$(basename $OUTPUT_DIR)/domain-pre-trained"
